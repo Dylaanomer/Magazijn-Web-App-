676388e3782e606f1904116ad10ca106cd70b70e
Dylaan Omer <glacialismercatus@gmail.com>
2022-07-28 11:29:03 +0200

    .....

--- ALPHA-DGS/Algemeen Applicatie Annotaties + Notities/Javascript Libraries in dit App.cs
+++ ALPHA-DGS/Algemeen Applicatie Annotaties + Notities/Javascript Libraries in dit App.cs
@@ -1,4 +1,16 @@
 ﻿/// JS LIBRARIES
 
-//
+//Bootstrap
+//jquery (3.6.0)
+//jquery-validation
+//jquery-validation-unobtrusive
+//selecg2
+
+
+///SCRIPTS
+//bootstrap.js
+//chosen.jquery.js
+//jquery-1.10.2.js
+//jquery-3.6.0.js
+//modernizr-2.6.2.js
 
