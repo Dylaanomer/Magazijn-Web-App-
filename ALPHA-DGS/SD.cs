fb6145f0472e423209dd914df5118091dd8ac7e4
Dylaan Omer <glacialismercatus@gmail.com>
2022-07-28 11:31:56 +0200

    SD ANNOTATIES

--- ALPHA-DGS/SD.cs
+++ ALPHA-DGS/SD.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace ALPHA_DGS
 {
+
+
+    //JS notificatie dat meld of SSO of een andere taak gelukt is of niet
+
     public static class SD
     {
         public const string Success = "Success";
